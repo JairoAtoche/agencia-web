f9d9e66bc6687ae28b4a7e06dd5dbd4195b45b1a
JairoAtoche <jairoatoche90098@gmail.com>
2023-09-26 20:48:18 -0500

    feat: Se agregó la definición de rutas

---
+++ src/routes/path.ts
@@ -0,0 +1,13 @@
+export enum ROUTES {
+	HOME = '/',
+	SERVICES = '/servicios',
+	PRICING = '/precios',
+	METHODOLOGY = '/metodologia',
+	PORTFOLIO = '/portafolio',
+	CONTACT_US = '/contactanos',
+	LEGAL_NOTICE = '/aviso-legal',
+	TERMS_CONDITIONS = '/terminos-condiciones',
+	PRIVACY_POLICY = '/politica-privacidad',
+	COOKIES_POLICY = '/politica-cookies',
+	COMPLAINTS_BOOK = '/libro-reclamaciones',
+}
