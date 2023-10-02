555e760e37e28f2b43e36a16a7f21ac7a3330d77
JairoAtoche <jairoatoche90098@gmail.com>
2023-10-01 21:24:48 -0500

    refactor: Actualizar  componentes layouts

--- src/consts/info.ts
+++ src/consts/info.ts
@@ -1,2 +1,4 @@
 export const WSP = '964 051 567';
+export const WSP_TRIM = WSP.replaceAll(' ', '');
+export const WSP_LINK = `https://www.wa.me/${WSP_TRIM}`;
 export const EMAIL = 'hola@devcode.com';
